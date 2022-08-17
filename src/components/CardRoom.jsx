099a4da3f7eda7b70f60f86250c4577949d71283
kriztofer3103 <cmromero1231gmail.com>
2022-08-16 20:16:59 -0500

    main home creado

--- src/components/CardRoom.jsx
+++ src/components/CardRoom.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 
-function CardRoom({ emoji, estadoPago, numeroCuarto, palabraId }) {
+function CardRoom({ numberRoom, numberEnglish, payMonth }) {
   return (
     <div className="accordion-item">
-      <h2 className="accordion-header" id={`flush-heading${palabraId}`}>
+      <h2 className="accordion-header" id={`flush-heading${numberEnglish}`}>
         <button
-          className="accordion-button collapsed fs-4"
+          className={`accordion-button collapsed fs-4 text-${
+            payMonth == true ? "success" : "danger"
+          }`}
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target={`#flush-collapse${palabraId}`}
+          data-bs-target={`#flush-collapse${numberEnglish}`}
           aria-expanded="false"
-          aria-controls={`flush-collapse${palabraId}`}
+          aria-controls={`flush-collapse${numberEnglish}`}
         >
-          <label className="">Cuarto {numeroCuarto}</label>
-          <span className={`bi bi-emoji-${emoji} mx-3`}></span>
-          <span className="position-absolute end-0 mx-5">{estadoPago}</span>
+          <label className="">Cuarto {numberRoom}</label>
+          <span
+            className={`bi bi-emoji-${
+              payMonth == true ? "smile" : "frown"
+            } mx-3`}
+          ></span>
+          <span className="position-absolute end-0 mx-5">
+            {payMonth == true ? "Actualizado" : "Deuda"}
+          </span>
         </button>
       </h2>
       <div
-        id={`flush-collapse${palabraId}`}
+        id={`flush-collapse${numberEnglish}`}
         className="accordion-collapse collapse"
-        aria-labelledby={`flush-heading${palabraId}`}
+        aria-labelledby={`flush-heading${numberEnglish}`}
         data-bs-parent="#accordionFlushExample"
       >
         <div className="accordion-body d-flex align-items-center">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,103 +1,31 @@
 import React from "react";
 import Footer from "../components/Footer";
 import CardRoom from "../components/CardRoom";
+import listRoom from "../json/listRoom.json";
 
 function HomePage() {
   return (
-    <div>
-      {/*       <Header /> */}
-      {/*       <div className="accordion accordion-flush" id="accordionFlushExample">
-        <CardRoom
-          emoji="smile"
-          estadoPago="Actualizado"
-          numeroCuarto="1"
-          palabraId="One"
-        />
-        <CardRoom
-          emoji="neutral"
-          estadoPago="Pendiente"
-          numeroCuarto="2"
-          palabraId="Two"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="3"
-          palabraId="Three"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="4"
-          palabraId="Four"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="5"
-          palabraId="Five"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="6"
-          palabraId="Six"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="7"
-          palabraId="Seven"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="8"
-          palabraId="Eight"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="9"
-          palabraId="Nine"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="10"
-          palabraId="Ten"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="11"
-          palabraId="Eleven"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="12"
-          palabraId="twelve"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="13"
-          palabraId="Thirteen"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="14"
-          palabraId="Fourthen"
-        />
-        <CardRoom
-          emoji="frown"
-          estadoPago="Deuda"
-          numeroCuarto="15"
-          palabraId="Fifteen"
-        />
-      </div> */}
+    <div className="bg-secondary">
+      {/* <Header /> */}
+
+      {/*Main*/}
+      <div
+        className="accordion accordion-flush mt-5 mb-5"
+        id="accordionFlushExample"
+      >
+        {listRoom.map((room) => {
+          return (
+            <CardRoom
+              key={room.numberRoom}
+              numberRoom={room.numberRoom}
+              numberEnglish={room.numberEnglish}
+              payMonth={room.payMonth}
+            />
+          );
+        })}
+      </div>
+
+      {/* Footer */}
       <Footer />
     </div>
   );
